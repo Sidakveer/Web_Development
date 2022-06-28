447fe57da8073b4f7941ab38fd823bcf8c6b08f0
Sidakveer <sidakveersingh8@gmail.com>
2022-06-28 12:46:25 -0400

    add changes

--- Starting+Files+-+flask-secrets/main.py
+++ Starting+Files+-+flask-secrets/main.py
@@ -1,13 +1,15 @@
+import wtforms.validators
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, InputRequired
 
 app = Flask(__name__)
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label="Email")
-    password = PasswordField(label="Password")
+    email = StringField(label="Email", validators=[Email()])
+    password = PasswordField(label="Password", validators=[InputRequired(message="Must be atleast 8 characters long")])
     submit = SubmitField(label="Log In")
 
 
@@ -20,9 +22,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    form.validate_on_submit()
     return render_template("login.html", form=form)
 
 
